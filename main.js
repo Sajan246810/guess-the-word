a82c748e40c0a86f9ede963ad3515a94b6a26051
Sajan246810 <76393636+Sajan246810@users.noreply.github.com>
2021-06-26 23:08:13 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,4 +5,5 @@ function login(){
     player2_name = document.getElementById("player_2_name_input").value ; 
     localStorage.setItem("player 1",player1_name);
     localStorage.setItem("player 2",player2_name);
+    window.location = "game_page.html";
 }
